17802a292144c32bddfec946cc6c0712199314a2
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2025-04-12 13:26:28 -0600

    connected to firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIn0JGnpqCVrKswO-7NOG6cS-S0JwjLMw",
+  authDomain: "artmarketplace-b9d82.firebaseapp.com",
+  projectId: "artmarketplace-b9d82",
+  storageBucket: "artmarketplace-b9d82.firebasestorage.app",
+  messagingSenderId: "45297249976",
+  appId: "1:45297249976:web:e588fc6e53a341a2c14e4d",
+  measurementId: "G-34J7ZWYFZR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
